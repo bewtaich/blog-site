efd6beb15347f43d5443758cc5c9a573dab94e4f
Bewtaich <bewtaich@gmail.com>
2024-07-14 18:45:12 -0500

    Creates blog html elements on submit

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,3 +1,21 @@
-const postData = localStorage.getItem('postData');
+const posts = JSON.parse(localStorage.getItem('posts'));
+const main = document.querySelector('main');
+const blogArea = document.createElement('div')
 
-console.log(JSON.parse(postData));
+
+main.appendChild(blogArea);
+
+for (let i=0; i < posts.length; i++){
+
+
+const blogTitle = document.createElement('h2');
+const blogUser = document.createElement('p');
+const blogContent = document.createElement('p');
+    blogArea.appendChild(blogUser);
+    blogArea.appendChild(blogTitle);
+    blogArea.appendChild(blogContent);
+
+    blogTitle.innerHTML=posts[i].title
+    blogUser.innerHTML=posts[i].user
+    blogContent.innerHTML=posts[i].content
+}
--- assets/js/form.js
+++ assets/js/form.js
@@ -3,23 +3,27 @@ const form=document.querySelector('form');
 const title = document.getElementById("title");
 const user = document.getElementById('user');
 const content = document.getElementById('content');
-
+const posts =[];
 
 
 const post = function (event) {
     event.preventDefault();
 
-    if (title.value || user.value || content.value ==="") {
+    if (title.value =="" || user.value=="" || content.value === "") {
         alert('Please fill out all fields');
+        console.log(`${title.value},${user.value},${content.value}`)
     } else {
 
     const postData = {
         user:user.value, 
         title:title.value,
-        contenet:content.value
+        content:content.value
     }     
 
-    localStorage.setItem('postData', JSON.stringify(postData))
+
+    posts.push(postData);
+
+    localStorage.setItem('posts', JSON.stringify(posts))
 
     location.href = 'blog.html'
     }

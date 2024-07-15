66f83108940b5170848bcaddd43285344cba06b8
Bewtaich <bewtaich@gmail.com>
2024-07-14 22:33:11 -0500

    Dynamic sizing

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,19 +1,21 @@
 const posts = JSON.parse(localStorage.getItem('posts'));
 const main = document.querySelector('main');
 const blogArea = document.createElement('div')
+blogArea.setAttribute('class', 'light')
 
 
-main.appendChild(blogArea);
 
 for (let i=0; i < posts.length; i++){
 
+    main.appendChild(blogArea);
 
-const blogTitle = document.createElement('h2');
-const blogUser = document.createElement('p');
-const blogContent = document.createElement('p');
-    blogArea.appendChild(blogUser);
+    const blogTitle = document.createElement('h2');
+    const blogUser = document.createElement('p');
+    const blogContent = document.createElement('p');
+ 
     blogArea.appendChild(blogTitle);
     blogArea.appendChild(blogContent);
+    blogArea.appendChild(blogUser);
 
     blogTitle.innerHTML=posts[i].title
     blogUser.innerHTML=posts[i].user
